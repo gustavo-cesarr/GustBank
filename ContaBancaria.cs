03cdb2cc2577bcc14144c16608c62f4a15994514
Gustavo Cesar <gustavo.silva1211@outlook.com>
2026-09-08 17:11:48 -0300

    Guardando o início do projeto no repositório remoto

---
+++ ContaBancaria.cs
@@ -0,0 +1,30 @@
+﻿//Aula 1 de Orientação a Objetos com C# - 2026
+
+//Exercício 1 - Conta bancária
+
+using System.Dynamic;
+class ContaBancaria
+{
+    public int NumeroIndicador {get; set;}
+    public string Titular {get; set;}
+    public double Saldo {get; set;}
+    public string Senha {get; set;}
+
+    public ContaBancaria (int numeroIndicador, string titular, double saldo, string senha)
+    {
+        NumeroIndicador = numeroIndicador;
+        Titular = titular;
+        Saldo = saldo;
+        Senha = senha;
+
+    }
+    public void MostrarConta()
+    {
+        Console.WriteLine($"Número da conta: {NumeroIndicador}");
+        Console.WriteLine($"Titular: {Titular}");
+        Console.WriteLine($"Saldo: {Saldo}");
+        Console.WriteLine($"Senha: {Senha}");
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,3 @@
+
+ContaBancaria conta1 = new ContaBancaria(1234, "João da Silva", 1000, "1234");
+conta1.MostrarConta();
